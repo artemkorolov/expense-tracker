4ae5aaa22a1bde079bb9640041e9a76ca30f5cc3
Artem Korolov <adkorolov@gmail.com>
2026-05-01 11:44:03 +0300

    feat: implement transaction object creation and array storage

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+let transactions = [];
+
+function addTransaction(event) {
+	event.preventDefault();
+
+	if (!(text instanceof HTMLInputElement) || !(amount instanceof HTMLInputElement)) {
+		return;
+	}
+
+	const transactionName = text.value.trim();
+	const transactionAmount = amount.value.trim();
+
+	if (transactionName == '' || transactionAmount == '') {
+		alert('Please enter the name and amount');
+		return;
+	}
+
+	const transaction = {
+		id: generateId(),
+		text: transactionName,
+		amount: +transactionAmount,
+	};
+
+	transactions.push(transaction);
+	console.log(transactions);
+
+	text.value = '';
+	amount.value = '';
+}
+
+form?.addEventListener('submit', addTransaction);
+
+function generateId() {
+	return Math.floor(Math.random() * 100000000);
+}
+
